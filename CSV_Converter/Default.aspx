9744307de8d990dee0a90a11ae509759448199c0
sidverma32 <sid.verma32@gmail.com>
2016-09-10 23:55:37 +0530

    Add files via upload

---
+++ CSV_Converter/Default.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+     <div>
+    <asp:FileUpload ID="FileUpload1" runat="server" />
+    <asp:Button ID="btnSubmit" runat="server" Text="Submit" OnClick="btnSubmit_Click"/>
+    </div>
+    <div>
+        <asp:GridView ID="gvDetails" runat="server">
+        </asp:GridView>
+    </div>
+    </form>
+</body>
+</html>
---
+++ CSV_Converter/Default.aspx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.IO;
+using Microsoft.VisualBasic.FileIO;
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnSubmit_Click(object sender, EventArgs e)
+    {
+        if (FileUpload1.HasFile)
+        {
+            string filename = FileUpload1.FileName.ToString();
+
+            string path = Server.MapPath("~/Document/" + filename);
+            FileUpload1.PostedFile.SaveAs(path);
+            FetchData(path);
+        }
+    }
+
+    protected void FetchData(string filepath)
+    {
+        DataTable dt = new DataTable();
+        bool IsFirstRowHeader = true;
+        string[] columnf = new string[] { "" };
+        using (TextFieldParser parser = new TextFieldParser(filepath))
+        {
+            parser.TrimWhiteSpace = true;
+            parser.TextFieldType = FieldType.Delimited;
+            parser.SetDelimiters(",");
+            if (IsFirstRowHeader)
+            {
+                columnf = parser.ReadFields();
+                foreach (string sds in columnf)
+                {
+                    DataColumn year = new DataColumn(sds.Trim().ToLower(), Type.GetType("System.String"));
+                    dt.Columns.Add(year);
+                }
+            }
+            while (true)
+            {
+                if (IsFirstRowHeader == false)
+                {
+                    string[] parts = parser.ReadFields();
+                    if (parts == null)
+                    {
+                        break;
+                    }
+                    dt.Rows.Add(parts);
+                }
+                IsFirstRowHeader = false;
+            }
+        }
+        gvDetails.DataSource = dt;
+        gvDetails.DataBind();
+    }
+}
